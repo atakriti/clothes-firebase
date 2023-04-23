5470eccd56df7dd5961a57945a68d650d6aea5ea
atakriti <anwart256@gmail.com>
2023-04-23 17:56:23 +0200

    update

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import "./footer.scss"
 import { BsGithub } from "react-icons/bs"
 import { MdEmail } from "react-icons/md"
-import {FcAndroidOs} from "react-icons/fc"
+import { FcAndroidOs } from "react-icons/fc"
+import app from "../clothes-app.apk"
 function Footer() {
   return (
       <footer>
           <h3>Developer by Anwar Takriti</h3>
           <div className='android'>
-                <a><FcAndroidOs/><h5>Get App</h5></a>
+                <a href={app} download={app} ><FcAndroidOs/><h5>Get App</h5></a>
           </div>
           <span>
               <a target={'_blank'} href="https://github.com/atakriti"><BsGithub /></a>
